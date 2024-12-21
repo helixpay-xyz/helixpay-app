aec051690153eb7c958fbe33571c3ffc39e5cf1c
misterbo271 <nevermind271999@gmail.com>
2024-12-21 09:14:49 +0700

    fix bug

--- app/screens/helixpay/auth/SeedPhrase.js
+++ app/screens/helixpay/auth/SeedPhrase.js
@@ -1,72 +1,83 @@
-import React from 'react';
-import {Keyboard} from 'react-native';
-import RootNavigation from 'screens/RootNavigation';
-import {CBImageBackground, CBInput, CBText, CBTouchableWithoutFeedback, CBView} from 'components';
-import { generateMnemonic, validateMnemonic } from '@scure/bip39';
+import React, {useState, useEffect, useRef, useContext, useMemo} from 'react';
+import { Keyboard } from 'react-native';
+import {
+    CBDivider,
+    CBImageBackground,
+    CBInput,
+    CBText, CBTouchableOpacity,
+    CBTouchableWithoutFeedback,
+    CBView,
+} from 'components';
+import Clipboard from '@react-native-clipboard/clipboard';
+import { generateMnemonic } from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
-import {appStyles} from 'configs/styles';
-import {strings} from 'controls/i18n';
+import { appStyles } from 'configs/styles';
+import { strings } from 'controls/i18n';
 import dimens from 'configs/dimens';
-import {ref, set } from "firebase/database";
-import {db} from 'app/firebase/config';
+import ImageUtil from 'utils/ImageUtil';
+import { FlashList } from '@shopify/flash-list';
+import colors from 'configs/colors';
+import Toast from "react-native-simple-toast";
 
-import Base from 'screens/Base';
-import EventTracker from 'controls/EventTracker';
-import ImageUtil from "utils/ImageUtil";
+const SeedPhrase = () => {
 
-export default class SeedPhrase extends Base {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            uri: '',
-            showPassword: false,
-            showConfirmPassword: false
-        };
-
-        this.cbBottomPassword = React.createRef();
-    }
-
-    componentDidMount() {
-        super.componentDidMount();
-        this.load();
-    }
-
-    componentWillUnmount() {
-        super.componentWillUnmount();
-    }
-
-    load() {
+    const seedPhraseArray = useMemo(() => {
         const generatedMnemonic = generateMnemonic(wordlist);
-    }
+        return generatedMnemonic.split(' ');
+    }, []);
 
-    onBlur = () => {
+
+    const onBlur = () => {
         Keyboard.dismiss();
     };
 
-    onClose = () => {
-        RootNavigation.goBack();
-        EventTracker.logEvent('screen_login', {action: 'click_button_back'});
+    const copyToClipboard = () => {
+        Clipboard.setString(seedPhraseArray.join(' '));
+        Toast.show(strings('text_copied_to_clipboard'), Toast.LONG);
     };
 
-    onNext = async (values) => {
-        await set(ref(db, 'phoneNumber/' + values?.phoneNumber), {
-            phoneNumber: values?.phoneNumber,
-        });
-        EventTracker.logEvent('screen_login', {action: 'click_next'});
+    const renderItem = ({ item, index }) => (
+        <CBView key={index} style={{width: dimens.widthScreen / 2 - 30, flexDirection: 'row', backgroundColor: 'rgba(255, 255, 255, 0.05)', paddingHorizontal: 30, paddingVertical: 10, borderRadius: 20, borderWidth: 1, borderColor: 'rgba(255, 255, 255, 0.05)'}}>
+            <CBText style={[appStyles.text, {textAlign: 'center', fontFamily: 'SpaceGrotesk-Bold', color: colors.white, flex: 1 }]} define={'text'}>
+                {index + 1}. {item}
+            </CBText>
+        </CBView>
+    );
+
+    const renderSeparator = () => {
+        return <CBView style={{height: 15}} define={'none'}/>;
     };
 
-    render() {
-        const {theme} = this.context;
-        return (
-            <CBImageBackground style={[{width: dimens.widthScreen, height: dimens.heightScreen, justifyContent: 'flex-start'}]} imageStyle={{width: dimens.widthScreen, height: dimens.heightScreen}} source={ImageUtil.getImage('background_1')}>
-                <CBTouchableWithoutFeedback style={{flex: 1}} define={'none'} onPress={this.onBlur}>
-                    <CBView style={[{flex: 1, paddingVertical: 15, paddingHorizontal: 15}, {borderTopLeftRadius: 30, borderTopRightRadius: 30, marginTop: dimens.statusBar}]}>
-                        <CBText style={[appStyles.heading, {marginTop: 60}]} define={'heading'}>{strings('text_secret_recovery_word')}</CBText>
-                        <CBText style={[appStyles.subtext, {marginTop: 5}]} define={'subtext'}>{strings('text_subtitle_srw')}</CBText>
-                    </CBView>
-                </CBTouchableWithoutFeedback>
-            </CBImageBackground>
-        );
-    }
-}
+    return (
+        <CBImageBackground style={{width: dimens.widthScreen, height: dimens.heightScreen, justifyContent: 'flex-start',}} imageStyle={{width: dimens.widthScreen, height: dimens.heightScreen,}} source={ImageUtil.getImage('background_1')}>
+            <CBTouchableWithoutFeedback style={{ flex: 1 }} define={'none'} onPress={onBlur}>
+                <CBView style={{flex: 1, paddingVertical: 15, paddingHorizontal: 15, borderTopLeftRadius: 30, borderTopRightRadius: 30, marginTop: dimens.statusBar,}}>
+                    <CBText style={[appStyles.heading, { marginTop: 60 }]} define={'heading'}>
+                        {strings('text_secret_recovery_word')}
+                    </CBText>
+                    <CBText style={[appStyles.subtext, { marginTop: 5 }]} define={'subtext'}>
+                        {strings('text_subtitle_srw')}
+                    </CBText>
+                    <FlashList
+                        contentContainerStyle={{paddingTop: 30}}
+                        showsVerticalScrollIndicator={false}
+                        keyboardDismissMode={'on-drag'}
+                        keyboardShouldPersistTaps={'always'}
+                        estimatedItemSize={12}
+                        data={seedPhraseArray}
+                        numColumns={2}
+                        renderItem={renderItem}
+                        ItemSeparatorComponent={renderSeparator}
+                    />
+                    <CBTouchableOpacity style={{padding: 15, borderBottomLeftRadius: 30, borderBottomRightRadius: 30,}} onPress={copyToClipboard}>
+                        <CBText style={[appStyles.text, {textAlign: 'center', fontFamily: 'SpaceGrotesk-Bold', color: colors.white,}]} define={'text'}>
+                            {strings('text_copy_to_clipboard')}
+                        </CBText>
+                    </CBTouchableOpacity>
+                </CBView>
+            </CBTouchableWithoutFeedback>
+        </CBImageBackground>
+    );
+};
+
+export default SeedPhrase;
